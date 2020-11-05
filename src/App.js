5f61c7b67abe267a40bbb24b0bf04cb128dcbdd1
judicaelandria <judiandriamahandry@gmail.com>
2020-11-05 18:43:26 +0300

    added new feature to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const [name, setName] = React.useState("Judicael");
+  React.useEffect(() => {
+    setTimeout(() => {
+      setName("Judicael Andriamahandry");
+    }, 4000);
+  }, [name]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>modification</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{name}</h1>
     </div>
   );
 }
